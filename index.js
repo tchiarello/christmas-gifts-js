0f2dd937ea583a8f4e3ace4528d668d4925557fd
Tatiane Chiarello <tatichiarello@gmail.com>
2019-12-04 12:26:52 +0100

    changing js document

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-/* Entrega dos presentes de natal. Nesse natal eu gostaria de enviar presentes para meus amigos. 
-O único problema é a logística, pois tenho amigos que moram em outras cidades e países.
-A relação dos lugares/presentes é:
-
--Brasil
-    Caxias do Sul: Uma camiseta branca
-    Porto Alegre: Uma camiseta azul
-    São Paulo: Uma camiseta amarela
--Austrália: Uma camiseta verde
--Estados Unidos: Uma camiseta roxa
--Londres: Uma camiseta preta
-
-O que deve ser feito?
--Desenhar o fluxograma do projeto no papel
--HTML
--Javascript
--O CSS não é necessário, só se você quiser
--Bônus: Utilizar arrays, função e switch case
--Bônus: Adicionar imagens dos presentes */
-
 function addGiftPicture(src, containerId) {
     let picture = document.createElement('img');
     picture.src = src;
